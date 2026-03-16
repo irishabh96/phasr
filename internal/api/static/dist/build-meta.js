1c76f2b7adb08a7940d215b575bd620138ab135f
Rishabh Bhatia <irishabh96@gmail.com>
2026-03-16 22:07:35 +0530

    fix

--- internal/api/static/dist/build-meta.js
+++ internal/api/static/dist/build-meta.js
@@ -1,2 +1,2 @@
-window.__STAQ_UI_BUILD_META__ = {"version":"0.2.17","builtAt":"2026-03-16T16:16:36.912Z","commit":"561144b"};
+window.__STAQ_UI_BUILD_META__ = {"version":"0.2.18","builtAt":"2026-03-16T16:26:06.692Z","commit":"561144b"};
 window.__STAQ_ASSET_VERSION__ = String(window.__STAQ_UI_BUILD_META__.version || "");
