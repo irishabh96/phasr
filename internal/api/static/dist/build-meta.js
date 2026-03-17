0af7f6d0d3a15c1697ae630bd167246c52c3f5b9
Rishabh Bhatia <irishabh96@gmail.com>
2026-03-17 15:01:41 +0530

    removed static files

--- internal/api/static/dist/build-meta.js
+++
@@ -1,2 +0,0 @@
-window.__PHASR_UI_BUILD_META__ = {"version":"0.2.77","builtAt":"2026-03-17T09:15:42.480Z","commit":"7f6e986"};
-window.__PHASR_ASSET_VERSION__ = String(window.__PHASR_UI_BUILD_META__.version || "");
